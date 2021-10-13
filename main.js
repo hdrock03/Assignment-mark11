89e918314642b842475857d18c479e2b66ce1438
hdrock03 <hardik.1rn16ec036@gmail.com>
2021-10-13 20:51:04 +0530

    feat changed style

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-const dateOfBirth = document.querySelector("#date-of-birth");
-const luckyDate = document.querySelector("#lucky-Date");
+const dob = document.querySelector("#date-of-birth");
+const luckyNumber = document.querySelector("#lucky-number");
 const check = document.querySelector("#calculate-number");
-const displayMessage = document.querySelector("#output-container");
+const output = document.querySelector("#output-container");
 
 const calculateSum = (date) => {
   let sum = 0;
@@ -15,18 +15,18 @@ const calculateSum = (date) => {
 const checkIsNumberLucky = (sumOfDate, numberToCheck) => {
   console.log(sumOfDate, numberToCheck);
   if (sumOfDate % numberToCheck === 0) {
-    return showMessage(`${numberToCheck} is a lucky number `);
+    return showMessage(`congrats ! ${numberToCheck} is a lucky number! `);
   }
-  showMessage(`${numberToCheck} is not lucky number`);
+  showMessage(`OOPS! ${numberToCheck} is not a lucky number!`);
 };
 
 const showMessage = (message) => {
-  displayMessage.innerText = message;
+  output.innerText = message;
 };
 
 check.addEventListener("click", () => {
-  const date = dateOfBirth.value;
-  const numberToCheck = luckyDate.value;
+  const date = dob.value;
+  const numberToCheck = luckyNumber.value;
   if (date && numberToCheck) {
     const sumOfDate = calculateSum(date);
     checkIsNumberLucky(sumOfDate, numberToCheck);
